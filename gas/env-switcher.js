92edaf910bca9a8ea74d77ee2e775836eeee475f
William Joslyn <121207299+wlj-iii@users.noreply.github.com>
2023-07-18 14:07:34 -0700

    whatever

---
+++ gas/env-switcher.js
@@ -0,0 +1,6 @@
+// FOR DEV USE ONLY
+/**
+ * THIS CODE IS MEANT TO EASE SWITCHING BETWEEN ENVIRONMENTS
+ * By which it is meant to copy the files labelled ' - Dev' to the Stage folder in GDrive
+ * Or vice versa, and when things are certain, it can also be used to do the same for Prod.
+ */
---
+++ gas/sandbox.js
@@ -0,0 +1,3 @@
+function iShouldntBeHere() {
+    ACC.charge("ascaddan2022@lakerschools.org", "missing", "Charger, Screen, Cable", 26, "Overdue")
+  }
